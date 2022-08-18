e3e111d427b56c55337eaf45a7a3008c3550d4b6
fandi nur <fandinur214@gmail.com>
2022-08-18 15:12:49 +0700

    Integrasi

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { UserContextProvider } from './context/UserContext';
 import App from './App';
+//Import QueryClient and QueryClientProvider
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <UserContextProvider>
+      {/* client */}
+    <QueryClientProvider client={client}>
       <App />
+      </QueryClientProvider>
+
     </UserContextProvider>
   </React.StrictMode>
 );
--- server/main.go
+++ server/main.go
@@ -7,20 +7,33 @@ import (
 	"waysbucks/pkg/mysql"
 	"waysbucks/routes"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	//initial DB
 	mysql.DatabaseInit()
 
+	//run migration
 	database.RunMigration()
 
 	r := mux.NewRouter()
 
 	routes.RouteInit(r.PathPrefix("/api/v1").Subrouter())
 
+	//path file
 	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
-	//try
-	fmt.Println("Server Running on localhost:5000")
-	http.ListenAndServe("localhost:5000", r)
+
+	// Setup allowed Header, Method, and Origin for CORS
+	var AllowedHeaders = handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	var AllowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "PATCH", "DELETE"})
+	var AllowedOrigins = handlers.AllowedOrigins([]string{"*"})
+
+	var port = "5000"
+
+	fmt.Println("Server Running on localhost:" + port)
+
+	// Embed the setup allowed in 2 parameter on this below code ...
+	http.ListenAndServe("localhost:"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
 }
