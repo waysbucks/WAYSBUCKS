c15e0daa40f714e17fa1324be4118de7f4a0bbc4
Kholish Nurfajri <kholisnurfajri1@gmail.com>
2022-08-20 20:40:57 +0700

    update transactions

--- server/dto/transaction/transaction.go
+++ server/dto/transaction/transaction.go
@@ -1,5 +1,7 @@
 package transactiondto
 
+import "waysbucks/models"
+
 type CreateTransaction struct {
 	UserID int `json:"user_id" form:"user_id" validate:"required"`
 }
@@ -10,8 +12,10 @@ type UpdateTransaction struct {
 	Total  int    `json:"total"`
 }
 
-// type TransactionResponse struct {
-// 	UserID    int `json:"user_id" form:"user_id"`
-// 	ProductID int `json:"product_id" form:"product_id"`
-// 	ToppingID int `json:"topping_id" form:"topping_id"`
-// }
+type TransactionResponse struct {
+	UserID    int                       `json:"user_id" form:"user_id"`
+	ProductID int                       `json:"product_id" form:"product_id"`
+	ToppingID int                       `json:"topping_id" form:"topping_id"`
+	Cart      []models.CartResponse     `json:"order"`
+	Product   models.ProductTransaction `json:"product"`
+}
